23bb34e244682fcd978e8bcaf8507bb535e5bffa
Kody Samaroo <kody.samaroo@gmail.com>
2020-11-24 17:24:15 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |k, v|
-    if k == :suite_a  && v.start_with?("A")
+    if k == :suite_a  && v[0] == "A"
       return v
     end
   end
